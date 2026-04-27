588223a593001ce092edf778c996bdf1b125bed9
lucastompsonl <lucastompson99@gmail.com>
2026-04-27 18:52:32 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ agendamentos/agendamentos/src/test/java/com/barbearia/agendamentos/AgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.agendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
